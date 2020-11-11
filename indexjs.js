ceaca89349d4df2ff0b00511233539c7805d1613
Antonio Martínez <antonio_martinez88@hotmail.com>
2020-11-11 17:19:31 -0400

    arreglo función random según longitud

--- indexjs.js
+++ indexjs.js
@@ -8,7 +8,7 @@ function myFunction() {
     //Who
     var who=new Array(5);
     who=["My mom", "My dog", "My girlfriend", "A mime", "An alien","Two Robots"];
-    var i=(Math.floor((Math.random() * 6))); //mostraría números del 0 al 5
+    var i=(Math.floor((Math.random() * who.length))); //mostraría números del 0 al 5
     //alert(i);
     //console.log(who[i]);
     //document.getElementById("excuse").innerHTML = who[i]+ " ";
@@ -16,7 +16,7 @@ function myFunction() {
     //Action
     var action=new Array(3);
     action=['ate','peed','crushed','broke'];
-    var j=(Math.floor((Math.random() * 4))); //mostraría números del 0 al 3
+    var j=(Math.floor((Math.random() * action.length))); //mostraría números del 0 al 3
     //alert(i);
     //console.log(who[i]);
     //document.getElementById("excuse").innerHTML = who[i]+ " " + action[j];
@@ -24,7 +24,7 @@ function myFunction() {
     //What
     var what=new Array(2);
     what=['my homework', 'the keys', 'the car'];
-    var k=(Math.floor((Math.random() * 3))); //mostraría números del 0 al 2
+    var k=(Math.floor((Math.random() * what.length))); //mostraría números del 0 al 2
     //alert(i);
     //console.log(who[i]);
     //document.getElementById("excuse").innerHTML = who[i]+ " " + action[j]+" "+what[k];
@@ -32,7 +32,7 @@ function myFunction() {
     //When
     var when=new Array(4);
     when=['before the class','right on time','when I finished','during my lunch','while I was praying'];
-    var l=(Math.floor((Math.random() * 5))); //mostraría números del 0 al 4
+    var l=(Math.floor((Math.random() * when.length))); //mostraría números del 0 al 4
     //alert(i);
     //console.log(who[i]);
     document.getElementById("excuse").innerHTML = who[i]+ " " + action[j]+" "+what[k]+" "+when[l];
